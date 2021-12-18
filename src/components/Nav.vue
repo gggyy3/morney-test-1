b3e346a8abbbb9486dfe59aa802fc8b52ca44753
gggyy3 <1587644910@qq.com>
2021-12-18 10:01:24 +0800

    html & css 写好了

--- src/components/Nav.vue
+++ src/components/Nav.vue
@@ -25,27 +25,26 @@ export default {
 </script>
 
 <style lang="scss" scoped>
-@import "~@/assets/style/helper.scss";
+@import '~@/assets/style/helper.scss';
 nav {
-    display:flex;
-    box-shadow: 0 0 3px rgba(0, 0, 0, 0.25);
-    flex-direction: row;
-    font-size: 12px;
-    > .item {
-        padding: 2px 0;
-        width: 33.33333%;
-        display: flex;
-        justify-content: center;
-        align-items: center;
-        flex-direction: column;
-        .icon{
-            width: 32px;
-            height: 32px;
-        }
+  @extend %outterShadow;
+  display: flex;
+  flex-direction: row;
+  font-size: 12px;
+  > .item {
+    padding: 2px 0;
+    width: 33.33333%;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    flex-direction: column;
+    .icon {
+      width: 32px;
+      height: 32px;
     }
-    > .item.selected{
-        color: $color-highlight;
-    }
+  }
+  > .item.selected {
+    color: $color-highlight;
+  }
 }
-
 </style>

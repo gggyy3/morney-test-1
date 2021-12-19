228a9e3bfb4a86108a7f1ef23b743465e3683d2e
gggyy3 <1587644910@qq.com>
2021-12-19 22:55:08 +0800

    基本完成 numberPad 组件

--- .eslintrc.js
+++ .eslintrc.js
@@ -15,6 +15,7 @@ module.exports = {
     'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
     'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
     '@typescript-eslint/no-var-requires': 0,
+    "@typescript-eslint/no-inferrable-types": "off", // 关闭类型判断
   },
   overrides: [
     {

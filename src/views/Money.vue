0f171c033b4a0a589d67e4f6e213702cc1bf969c
gggyy3 <1587644910@qq.com>
2021-12-20 11:33:15 +0800

    可以选中或取消 tag

--- src/views/Money.vue
+++ src/views/Money.vue
@@ -1,21 +1,25 @@
 <template>
   <Layout class-prefix="layout">
-    <Number-pad/>
-    <Types :xxx="333"/>
+    <Number-pad />
+    <Types :xxx="333" />
     <Notes/>
-    <Tags/>
-
+    <Tags :data-source="tags"/>
   </Layout>
 </template>
 
-<script lang="ts">
+<script>
 import NumberPad from '@/components/Money/NumberPad.vue'
 import Tags from '@/components/Money/Tags.vue'
 import Notes from '@/components/Money/Notes.vue'
 import Types from '@/components/Money/Types.vue'
 export default {
   components: { NumberPad, Tags, Notes, Types },
-  name: 'Money'
+  name: 'Money',
+  data() {
+    return {
+      tags: ['衣', '食', '住', '行']
+    }
+  }
 }
 </script>
 

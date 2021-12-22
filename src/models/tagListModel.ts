ee59e018ccd5822bf6b017d080c65b8c42bfae32
gggyy3 <1587644910@qq.com>
2021-12-22 11:03:18 +0800

    可以编辑和删除标签

--- src/models/tagListModel.ts
+++ src/models/tagListModel.ts
@@ -1,12 +1,14 @@
 const localStorageKeyName = "tagList";
 type Tag = {
-    id: string
-    name: string
-}
+  id: string;
+  name: string;
+};
 type TagListModel = {
   data: Tag[];
   fetch: () => Tag[];
-  create: (name: string) => 'success' | 'duplicated'; // 联合类型
+  create: (name: string) => "success" | "duplicated"; // 联合类型
+  update: (id: string, name: string) => "success" | "not found" | "duplicated";
+  remove: (id: string) => boolean;
   save: () => void;
 };
 const tagListModel: TagListModel = {
@@ -17,22 +19,47 @@ const tagListModel: TagListModel = {
     );
     return this.data;
   },
-    create(name) {
+  create(name) {
     // this.data = [{id:'1', name:'1'}, {id:'2', name: '2'}]
-    const names = this.data.map(item => item.name)
+    const names = this.data.map((item) => item.name);
     if (names.indexOf(name) >= 0) {
-        return 'duplicated'
-        }
+      return "duplicated";
+    }
 
-    this.data.push({id: name, name:name});
+    this.data.push({ id: name, name: name });
     this.save();
-    return 'success';
+    return "success";
+  },
+  update(id: string, name: string) {
+    const idList = this.data.map((item) => item.id);
+    if (idList.indexOf(id) >= 0) {
+      const names = this.data.map((item) => item.name);
+      if (names.indexOf(name) >= 0) {
+        return "duplicated";
+      } else {
+        const tag = this.data.filter((item) => item.id === id)[0];
+        tag.name = name;
+        this.save();
+        return "success";
+      }
+    } else {
+      return "not found";
+    }
+  },
+  remove(id: string) {
+    let index = -1;
+    for (let i = 0; i < this.data.length; i++) {
+      if (this.data[i].id === id) {
+        index = i;
+        break;
+      }
+    }
+    this.data.splice(index, 1);
+    this.save();
+    return true;
   },
   save() {
-    window.localStorage.setItem(
-      localStorageKeyName,
-      JSON.stringify(this.data)
-    );
+    window.localStorage.setItem(localStorageKeyName, JSON.stringify(this.data));
   },
 };
 

c6fa02efd86679f848df77fb8accb17d9213f660
gggyy3 <1587644910@qq.com>
2021-12-25 15:31:52 +0800

    EditLabel.vue 使用 Vuex

--- src/views/EditLabel.vue
+++ src/views/EditLabel.vue
@@ -7,7 +7,7 @@
     </div>
     <div class="from-warpper">
       <FormItem
-        :value="tag.name"
+        :value="currentTag.name"
         @update:value="update"
         field-name="标签名"
         placeholder="请输入标签名"
@@ -29,27 +29,31 @@ import Button from '@/components/Button.vue'
   components: { Button, FormItem }
 })
 export default class EditLabel extends Vue {
-  tag?: Tag = undefined
+  get currentTag() {
+    return this.$store.state.currentTag
+  }
 
   created() {
-    // this.tag = store.findTag(this.$route.params.id)
-    if (!this.tag) {
+    const id = this.$route.params.id
+    this.$store.commit('fetchTags')
+    this.$store.commit('setCurrentTag', id)
+    if (!this.currentTag) {
       this.$router.replace('/404')
     }
   }
+
   update(name: string) {
-    if (this.tag) {
-      // store.updateTag(this.tag.id, name)
+    if (this.currentTag) {
+      this.$store.commit('updateTag', {
+        id: this.currentTag.id,
+        name
+      })
     }
   }
+
   remove() {
-    if (this.tag) {
-      return
-      // if (store.removeTag(this.tag.id)) {
-      //   this.$router.back()
-      // } else {
-      //   window.alert('删除失败')
-      // }
+    if (this.currentTag) {
+      this.$store.commit('removeTag', this.currentTag.id)
     }
   }
   goBack() {
--- src/views/Money.vue
+++ src/views/Money.vue
@@ -9,39 +9,38 @@
         @update:value="onUpdateNotes"
       />
     </div>
-    <Tags/>
+    <Tags />
   </Layout>
 </template>
 
 <script lang="ts">
-import Vue from 'vue'
-import NumberPad from '@/components/Money/NumberPad.vue'
-import Tags from '@/components/Money/Tags.vue'
-import FormItem from '@/components/Money/Formitem.vue'
-import Types from '@/components/Money/Types.vue'
-import { Component } from 'vue-property-decorator'
+import Vue from "vue";
+import NumberPad from "@/components/Money/NumberPad.vue";
+import Types from "@/components/Money/Types.vue";
+import FormItem from "@/components/Money/Formitem.vue";
+import Tags from "@/components/Money/Tags.vue";
+import { Component } from "vue-property-decorator";
 
 @Component({
   components: { Tags, FormItem, Types, NumberPad },
-  computed: {
-    count() {
-      return this.$store.state.recordList
-    }
-  }
 })
 export default class Money extends Vue {
-  record: RecordItem = { tags: [], notes: '', type: '-', amount: 0 }
+  get recordList() {
+    return this.$store.state.recordList;
+  }
 
-  created(){
-    this.$store.commit('fetchRecords')
+  record: RecordItem = { tags: [], notes: "", type: "-", amount: 0 };
+
+  created() {
+    this.$store.commit("fetchRecords");
   }
 
   onUpdateNotes(value: string) {
-    this.record.notes = value
+    this.record.notes = value;
   }
 
   saveRecord() {
-    this.$store.commit('createRecord', this.record)
+    this.$store.commit("createRecord", this.record);
   }
 }
 </script>
